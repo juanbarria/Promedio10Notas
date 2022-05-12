6f4ecdea1c0a73c9f02a78604a7069dd9cf8863f
Juan Manuel Barria <103613099+juanbarria@users.noreply.github.com>
2022-05-12 18:08:47 -0300

    correccion variable number

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-let nota, suma, promedio, contador : number;
-contador =1;
+let nota, suma, promedio, contador; //number;
+contador = 1;
 suma = 0;
-while (contador<=10) {
-   nota= Number(prompt("Ingrese una nota: "));
-   suma = suma + nota;
-   contador = contador +1;
+while (contador <= 10) {
+  nota = Number(prompt("Ingrese una nota: "));
+  suma = suma + nota;
+  contador = contador + 1;
 }
-promedio = suma/10;
+promedio = suma / 10;
 console.log("El promedio de las notas es: " + promedio);
